38bbbcc05904f849390a57d5fa97df5152a2ee26
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-09-07 12:24:58 +0300

    Added one-activity pattern

--- app/src/main/java/by/matthewvirus/medicinenotifier/ui/activities/HomeActivity.kt
+++ app/src/main/java/by/matthewvirus/medicinenotifier/ui/activities/HomeActivity.kt
@@ -3,10 +3,21 @@ package by.matthewvirus.medicinenotifier.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import by.matthewvirus.medicinenotifier.R
+import by.matthewvirus.medicinenotifier.ui.patientslist.PatientsListFragment
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null) {
+            val fragment = PatientsListFragment.newInstance()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
