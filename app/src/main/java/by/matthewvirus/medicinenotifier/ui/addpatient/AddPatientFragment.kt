69735836dae56d4ae61da4e2eb6eabe82014c872
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-09-07 12:30:04 +0300

    Created simple ui

--- app/src/main/java/by/matthewvirus/medicinenotifier/ui/addpatient/AddPatientFragment.kt
+++ app/src/main/java/by/matthewvirus/medicinenotifier/ui/addpatient/AddPatientFragment.kt
@@ -14,19 +14,17 @@ class AddPatientFragment : Fragment() {
         fun newInstance() = AddPatientFragment()
     }
 
-    private lateinit var viewModel: AddPatientViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.add_patient_fragment, container, false)
+    private val addPatientViewModel by lazy {
+        ViewModelProvider(this).get(AddPatientViewModel::class.java)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AddPatientViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        val view =  inflater.inflate(R.layout.add_patient_fragment, container, false)
+        return view
     }
 
 }
