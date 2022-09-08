0bf52a4854e457ff0b36adf8be7ffce64d52dd40
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-09-08 14:55:59 +0300

    Patient model changed a bit

--- app/src/main/java/by/matthewvirus/medicinenotifier/data/datamodel/PatientDataModel.kt
+++ app/src/main/java/by/matthewvirus/medicinenotifier/data/datamodel/PatientDataModel.kt
@@ -6,5 +6,8 @@ data class PatientDataModel(
     var patientId: UUID = UUID.randomUUID(),
     var patientName: String = "",
     var patientSurname: String = "",
+    var patientPatronymic: String = "",
+    var patientDiagnosis: String = "",
+    var dateOfBirth: Date = Date(),
     var patientStatus: Boolean = false
 )
