0788463bfd02b083bfda021bb43207cbcd1a220f
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-09-11 00:22:13 +0300

    Added application layer for database singleton

---
+++ app/src/main/java/by/matthewvirus/medicinenotifier/MedicineNotifierApplication.kt
@@ -0,0 +1,11 @@
+package by.matthewvirus.medicinenotifier
+
+import android.app.Application
+
+class MedicineNotifierApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
