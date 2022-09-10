905a6149bbefb6f832f9a8b67bf41f5eb250058b
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-09-11 00:23:24 +0300

    Extracted constants as util file

--- app/src/main/java/by/matthewvirus/medicinenotifier/util/Constants.kt
+++ app/src/main/java/by/matthewvirus/medicinenotifier/util/Constants.kt
@@ -1,6 +1,6 @@
 package by.matthewvirus.medicinenotifier.util
 
 const val DIALOG_DATE = "DialogDate"
-const val REQUEST_DATE = 0
-const val DATE_PATTERN = "d MMMM y"
 const val ARG_DATE = "date"
+const val DATE_FORMAT = "d MMMM y"
+const val REQUEST_DATE = 0
