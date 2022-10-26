978100943d5e77c88e001a858aca56007de16a18
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-10-27 00:41:24 +0300

    Little refactoring.

--- app/src/main/java/by/matthewvirus/medicinenotifier/data/database/dao/MedicineDao.kt
+++ app/src/main/java/by/matthewvirus/medicinenotifier/data/database/dao/MedicineDao.kt
@@ -2,7 +2,6 @@ package by.matthewvirus.medicinenotifier.data.database.dao
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import by.matthewvirus.medicinenotifier.data.datamodel.MedicineDataModel
