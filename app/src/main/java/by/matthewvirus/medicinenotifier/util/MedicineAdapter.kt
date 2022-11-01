941854e468fbd07c23df62e3da55b97ee8614cdf
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-11-01 23:04:22 +0300

    Implemented UI features

--- app/src/main/java/by/matthewvirus/medicinenotifier/util/MedicineAdapter.kt
+++ app/src/main/java/by/matthewvirus/medicinenotifier/util/MedicineAdapter.kt
@@ -1,10 +1,14 @@
 package by.matthewvirus.medicinenotifier.util
 
 import android.annotation.SuppressLint
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.widget.ImageViewCompat
 import androidx.recyclerview.widget.RecyclerView
 import by.matthewvirus.medicinenotifier.R
 import by.matthewvirus.medicinenotifier.data.datamodel.MedicineDataModel
@@ -27,13 +31,25 @@ class MedicineAdapter(
         private val medicineNameTitle: TextView = itemView.findViewById(R.id.medicine_item_name)
         private val medicineTakingTime: TextView = itemView.findViewById(R.id.medicines_times)
         private val medicineNumberInContainerTitle: TextView = itemView.findViewById(R.id.medicine_item_number_in_container)
+        private val medicineStatusImageView: ImageView = itemView.findViewById(R.id.medicine_active_status)
 
         @SuppressLint("SetTextI18n", "SimpleDateFormat")
         fun bind(medicine: MedicineDataModel) {
             this.medicine = medicine
             medicineNameTitle.text = this.medicine.medicineName
             medicineTakingTime.text = this.medicine.medicineUseTimesPerDay + itemView.context.getString(R.string.first_notification) + SimpleDateFormat(TIME_FORMAT).format(this.medicine.medicineTakingFirstTime)
-            medicineNumberInContainerTitle.text = itemView.context.getString(R.string.medicines_left) + this.medicine.medicineNumberInContainer.toString()
+            medicineNumberInContainerTitle.text = itemView.context.getString(R.string.medicines_left) + " " + this.medicine.medicineNumberInContainer.toString()
+            when (medicine.medicineStatus) {
+                0 -> {
+                    medicineStatusImageView.setImageResource(R.drawable.ic_notif_off)
+                    ImageViewCompat.setImageTintList(medicineStatusImageView, ColorStateList.valueOf(Color.parseColor("#ff5722")))
+                }
+                else -> {
+                    medicineStatusImageView.setImageResource(R.drawable.ic_notif_on)
+                    ImageViewCompat.setImageTintList(medicineStatusImageView, ColorStateList.valueOf(Color.parseColor("#8bc34a")))
+
+                }
+            }
         }
     }
 
