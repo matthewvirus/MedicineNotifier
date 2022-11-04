581609e47d152308818289a1f1e9b5f61c486aa3
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-11-04 18:16:25 +0300

    [New feature] Open the app when notification clicked

--- app/src/main/java/by/matthewvirus/medicinenotifier/receivers/AlarmReceiver.kt
+++ app/src/main/java/by/matthewvirus/medicinenotifier/receivers/AlarmReceiver.kt
@@ -7,13 +7,13 @@ import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import by.matthewvirus.medicinenotifier.R
-import by.matthewvirus.medicinenotifier.ui.addMedicine.AddMedicineFragment
+import by.matthewvirus.medicinenotifier.ui.activities.HomeActivity
 import by.matthewvirus.medicinenotifier.util.*
 
 class AlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        val notificationIntent = Intent(context, AddMedicineFragment::class.java)
+        val notificationIntent = Intent(context, HomeActivity::class.java)
         val checker = intent?.extras?.getString("id")
         notificationIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         val pendingIntent = PendingIntent.getActivity( context,0, notificationIntent,PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
