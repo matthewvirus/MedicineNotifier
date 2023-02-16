0b080362305ef27b06b4ae274fbf5b3d87086e8f
Matthew Zhigalo <matthewzhigalo@gmail.com>
2023-02-16 20:10:01 +0300

    Created new converters

---
+++ app/src/main/java/by/matthewvirus/medicinenotifier/data/domain/dataconverters/Converters.kt
@@ -0,0 +1,19 @@
+package by.matthewvirus.medicinenotifier.data.domain.dataconverters
+
+import androidx.room.TypeConverter
+import java.util.*
+
+class Converters {
+
+    @TypeConverter
+    fun fromDate(date: Date?): Long? {
+        return date?.time
+    }
+
+    @TypeConverter
+    fun toDate(millisSinceEpoch: Long?): Date? {
+        return millisSinceEpoch?.let {
+            Date(it)
+        }
+    }
+}
