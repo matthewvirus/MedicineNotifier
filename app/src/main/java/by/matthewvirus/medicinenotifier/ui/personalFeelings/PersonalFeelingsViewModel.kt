69a3444b1fc530579065a6d00436cd05306b8fcb
Matthew Zhigalo <matthewzhigalo@gmail.com>
2023-02-16 20:03:13 +0300

    Removed unused files

--- app/src/main/java/by/matthewvirus/medicinenotifier/ui/personalFeelings/PersonalFeelingsViewModel.kt
+++
@@ -1,7 +0,0 @@
-package by.matthewvirus.medicinenotifier.ui.personalFeelings
-
-import androidx.lifecycle.ViewModel
-
-class PersonalFeelingsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
