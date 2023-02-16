b64231a0976699a4e3fce5b70f9ecbd93dd9546e
Matthew Zhigalo <matthewzhigalo@gmail.com>
2023-02-16 20:10:35 +0300

    Refactored

--- app/src/main/java/by/matthewvirus/medicinenotifier/util/Constants.kt
+++ app/src/main/java/by/matthewvirus/medicinenotifier/util/Constants.kt
@@ -1,11 +1,5 @@
 package by.matthewvirus.medicinenotifier.util
 
-// DatePickerDialog constants
-const val DIALOG_DATE = "DialogDate"
-const val ARG_DATE = "date"
-const val DATE_FORMAT = "d MMMM y"
-const val REQUEST_DATE = 0
-
 // TimePickerDialog constants
 const val DIALOG_TIME = "DialogTime"
 const val ARG_TIME = "time"
